4cfc2d71dbab5ad53b6aac98f1ee3b0b247188aa
SHINCY SUNNY PS <shincynov2@gmail.com>
2025-07-24 10:59:49 +0530

    Delete timerr.py

--- timerr.py
+++
@@ -1,8 +0,0 @@
-import time
-my=int(input("time="))
-for i in range(my,0,-1):
-    sec=i%60
-    min=int((i/60)%60)
-    hour=int(i/3600)
-    print(f"{hour:02}:{min:02}:{sec:02}")
-    time.sleep(10)
