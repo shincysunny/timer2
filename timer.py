764d0dfe986eaa9c04543b03faae61cfca6015f5
shincysunny <shincynov2@gmail.com>
2025-07-24 11:02:26 +0530

    Initial commit

---
+++ timer.py
@@ -0,0 +1,8 @@
+import time
+my=int(input("time="))
+for i in range(my,0,-1):
+    sec=i%60
+    min=int((i/60)%60)
+    hour=int(i/3600)
+    print(f"{hour:02}:{min:02}:{sec:02}")
+    time.sleep(1)
